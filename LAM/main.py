494f2c895445b7f6bf79c19135ac6eae0aeefdad
CodCodingCode <nathanyan2008p@gmail.com>
2026-09-11 20:00:18 -0400

    added shi

---
+++ LAM/main.py
@@ -0,0 +1,21 @@
+'''Input is batches, frames, (64, 64, 3)
+A = 8
+Frames = ?
+Spatial tokens
+
+
+'''
+
+'''
+Middle is just the ST transformer (spatial then temporal)
+
+'''
+
+
+
+
+'''
+Output is 18 outpu
+18 total actions. 
+
+'''
